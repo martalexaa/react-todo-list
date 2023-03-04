63f7b6500a5583db5e1a0f65f3d96b620b601ce0
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-03-04 14:16:09 +0100

    Updates

---
+++ precache-manifest.b3207ce1507732857992f994625f7ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "994616ef028b30301aca",
+    "url": "/react-todo-list/static/css/main.92091526.chunk.css"
+  },
+  {
+    "revision": "994616ef028b30301aca",
+    "url": "/react-todo-list/static/js/main.994616ef.chunk.js"
+  },
+  {
+    "revision": "db131c07163c3e56e454",
+    "url": "/react-todo-list/static/js/1.db131c07.chunk.js"
+  },
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "f7c5a694d8e8a270835fe457d5539e53",
+    "url": "/react-todo-list/index.html"
+  }
+];
